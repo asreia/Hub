f6d27f78d30cfb25f99ea4313b7181d725aa9ec9
asreia <asreia_0@yahoo.co.jp>
2022-07-21 19:07:31 +0900

    haskell

--- Kenron/haskelltest.hs
+++ Kenron/haskelltest.hs
@@ -1,1 +1,26 @@
-main = putStrLn "Hello World"
+-- {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+main :: IO()
+main = do
+    print ""
+    -- print $ map' (*2) [1, 2, 3, 4]
+    -- print $ and' [True,True,True]
+    print $ maximum' [2]
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+and' :: [Bool] -> Bool
+and' [] = False
+and' [True] = True
+and' (False:xs) = False
+and' (True:xs) = and' xs
+
+maximum' :: Ord a => [a] -> a
+maximum' [] = errorWithoutStackTrace "error"
+maximum' [x] = x
+maximum' (x:y:xs) 
+    | x < y =  maximum' (y:xs)
+    | otherwise = maximum' (x:xs)
+b = maximum[1,2,3,4]
+a = and [True,True,True]
