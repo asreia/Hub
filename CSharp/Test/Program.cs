b4afd4c552153c54e008363aa13d9ad9cc21b767
asreia <asreia_0@yahoo.co.jp>
2022-08-21 09:36:52 +0900

    recode

---
+++ CSharp/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+Console.WriteLine(new Person("namae", default).Func());
+
+
+
+record Person(string Name, DateTime Birthday)
+{
+    public Person() : this("", default(DateTime)) { }
+    public string Func() //関数が書けない訳ではない
+    {
+        Console.WriteLine("Func");
+        return "abc";
+    }
+}
